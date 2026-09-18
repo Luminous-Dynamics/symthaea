4047a12724d00985128f2dd00c049432abed2a04
Tristan Stoltz <tristan.stoltz@evolvingresonantcocreationism.com>
2026-09-18 12:09:10 +0200

    feat(knowledge): expose mutation seal wire envelope

--- src/knowledge/mod.rs
+++ src/knowledge/mod.rs
@@ -36,6 +36,7 @@ mod belief_mutation_decision_guard;
 mod belief_mutation_firewall;
 mod belief_mutation_persistence;
 pub mod belief_mutation_seal_persistence;
+pub mod belief_mutation_seal_wire;
 mod belief_mutation_transaction;
 mod belief_mutation_verifier;
 pub mod belief_revision_gate;
