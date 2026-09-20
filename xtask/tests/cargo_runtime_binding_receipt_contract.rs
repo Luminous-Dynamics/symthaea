b76bb1ba11f00a730ae53194d21870ccf8941b4a
Tristan Stoltz <tristan.stoltz@evolvingresonantcocreationism.com>
2026-09-20 15:59:12 +0200

    ENV-EXEC-001I: verify persisted Cargo runtime bindings through canonical rebuild

---
+++ xtask/tests/cargo_runtime_binding_receipt_contract.rs
@@ -0,0 +1,16 @@
+#![allow(dead_code)]
+
+#[path = "../src/cargo_adapter_semantics.rs"]
+mod cargo_adapter_semantics;
+#[path = "../src/cargo_context.rs"]
+mod cargo_context;
+#[path = "../src/cargo_context_verify.rs"]
+mod cargo_context_verify;
+#[path = "../src/repository_snapshot_receipt.rs"]
+mod repository_snapshot_receipt;
+#[path = "../src/repository_effect_policy.rs"]
+mod repository_effect_policy;
+#[path = "../src/cargo_runtime_binding.rs"]
+mod cargo_runtime_binding;
+#[path = "../src/cargo_runtime_binding_receipt.rs"]
+mod cargo_runtime_binding_receipt;
